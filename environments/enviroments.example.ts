c5538308fa0c40dc104bb68ddbbbfb1d1b5555cb
Alexis Herrera Saucedo <alexishs451@gmail.com>
2025-07-22 22:44:51 -0600

    chore: permite versionar environments.enviroments.example.ts como plantilla de entorno

---
+++ environments/enviroments.example.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false, // Cambia a true en producción
+  firebaseConfig: {
+    apiKey: "<TU_API_KEY>",
+    authDomain: "<TU_PROJECT_ID>.firebaseapp.com",
+    projectId: "<TU_PROJECT_ID>",
+    storageBucket: "<TU_PROJECT_ID>.appspot.com",
+    messagingSenderId: "<TU_MESSAGING_SENDER_ID>",
+    appId: "<TU_APP_ID>"
+  }
+};
+// Renombra este archivo a enviroments.ts y coloca tus credenciales reales para desarrollo local. 
